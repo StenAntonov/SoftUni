4c251d43c98907d7850c81d3acdc058e74bc6203
StenAntonov <74548851+StenAntonov@users.noreply.github.com>
2021-07-15 14:47:10 +0300

    Delete Simple Operations and Calculations directory

--- Simple Operations and Calculations/Inches to Centimeters.js
+++
@@ -1,6 +0,0 @@
-function inchToCent(input) {
-    let inches = Number(input.shift());
-    let cm = inches * 2.54;
-    console.log(cm.toFixed(2));
-}
-inchToCent([5])
--- Simple Operations and Calculations/Project.js
+++
@@ -1,7 +0,0 @@
-function project(input) {
-    let name = input.shift();
-    let  number = Number(input.shift());
-    let time = number * 3
-    console.log("The architect " + name + " will need " + time + " hours to complete " + number + " project/s." );
-}
-project(["Pesho",10])
