d66e845c6d53105aa167e1b76d60a209c06cd2c2
StenAntonov <74548851+StenAntonov@users.noreply.github.com>
2021-11-23 13:48:41 +0200

    Delete JS Advanced/Advanced Functions/Unit Testing and Error Handling directory

--- JS Advanced/Advanced Functions/Unit Testing and Error Handling/app.js
+++
@@ -1,8 +0,0 @@
-function notify(message) {
-const notification = document.getElementById('notification');
-notification.textContent = message;
-notification.style.display = 'block';
-notification.addEventListener('click', () => {
-  notification.style.display = 'none';
-});
-}
